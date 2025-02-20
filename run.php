114aac698298188644364a84d2c2c02d4bf4f244
l.dogaru <l.dogaru@sportradar.com>
2025-02-20 10:15:36 +0100

    Added the Environment variable

--- run.php
+++ run.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'Interpreter.php';
+require_once 'Environment.php';
 
 
 $fileContent = file_get_contents('./index.oida');
-$interpreter = new Interpreter($fileContent);
+$env = new Environment();
+$interpreter = new Interpreter($fileContent, $env);
 
 $interpreter->evaluate();
 
